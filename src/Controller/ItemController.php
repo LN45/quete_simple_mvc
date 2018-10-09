7037f15921948ab4d69248d3de58e3361ba66526
Helene Bloch <ln-45@live.fr>
2018-10-09 14:39:53 +0200

    add files quete4 mvc

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -9,20 +9,33 @@
 // src/Controller/ItemController.php
 namespace Controller;
 use Model\ItemManager;
+// ... ajoute ces 2 use
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 
+class ItemController
+{
+    private $twig;
 
-class ItemController{
-    public function index (){
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+        $this->twig = new Twig_Environment($loader);
+    }
+
+    public function index()
+    {
         $itemManager = new ItemManager();
         $items=$itemManager->selectAllItems();
-        require __DIR__ . '/../View/Item.php';
+        return $this->twig->render('Item.html.twig', ['items' => $items]);
     }
+
     public function show(int $id)
     {
         $itemManager = new ItemManager();
         $item = $itemManager->selectOneItem($id);
-
-        require __DIR__ . '/../View/showItem.php';
+        return $this->twig->render('showItem.html.twig', ['item' => $item]);
     }
 }
+
